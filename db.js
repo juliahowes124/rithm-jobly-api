db4e2ae398812fad2a9662eca67993f471583140
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-04-15 13:41:55 -0700

    different db config depending on node_env

--- db.js
+++ db.js
@@ -1,16 +1,23 @@
 "use strict";
 
+require("dotenv").config();
 /** Database setup for jobly. */
 
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
-const db = new Client({
+const db = process.env.NODE_ENV === "production"
+? new Client({
   connectionString: getDatabaseUri(),
   ssl: {
     rejectUnauthorized: false
   }
-});
+})
+:
+new Client({
+  connectionString: getDatabaseUri()
+})
+
 
 db.connect();
 
